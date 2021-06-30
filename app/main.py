c880088514cdabc22729097d4dea41cc46c4aa6c
jackcasey067 <jackcasey067@gmail.com>
2021-06-30 00:11:16 -0500

    Initial Commit

---
+++ app/main.py
@@ -0,0 +1,10 @@
+
+# Following the tutorial here: https://www.geeksforgeeks.org/deploy-python-flask-app-on-heroku/
+
+from flask import Flask
+  
+app = Flask(__name__)
+  
+@app.route("/")
+def home_view():
+    return "<h1>Welcome to Geeks for Geeks</h1>"
