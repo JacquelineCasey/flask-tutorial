30433d89503e348b25c1d4cf575c13929889007f
jackcasey067 <jackcasey067@gmail.com>
2021-06-30 15:28:44 -0500

    Updated gitignore; removed wsgi.py and moved driver code to app/main.py, changed Procfile to run gunicorn directly on that file.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
-
 # Following the tutorial here: https://www.geeksforgeeks.org/deploy-python-flask-app-on-heroku/
 
 from flask import Flask
+
+
+# This app uses the functions with the @app.route() decorator.
+# Additionally, Procfile tells gunicorn to run this variable.
+app = Flask(__name__) 
   
-app = Flask(__name__)
   
+# Homepage
 @app.route("/")
-def home_view():
+def home_page():
+    return "<h1> This is the homepage. </h1>"
+
+# Old Homepage (Add /deprecated to url)
+@app.route("/deprecated")
+def old_view():
     return "<h1>Welcome to my website! :D </h1>"
+
+
+# Runs the app
+if __name__ == "__main__":
+    app.run()
