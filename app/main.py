94ef3ca6970d2831670595563e324afab3a8b1bf
jackcasey067 <jackcasey067@gmail.com>
2021-07-01 17:09:36 -0500

    Added a VERY RUDIMENTARY Password system: - Navigate to /secret to try it - Navigate to /logout to undo it - I still need to:   - Write up what I have learned in the README   - Clean up the code a little   - Generate a better key and password, stored in .env   - Figure out if I can redirect back to any page, not just /secret   - Move sent HTML to seperate HTML Files.

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 # Following the tutorial here: https://www.geeksforgeeks.org/deploy-python-flask-app-on-heroku/
 
-from flask import Flask, render_template, url_for, redirect
-import os # For checking environment variables.
-
+from flask import Flask, render_template, url_for, redirect, session, request
+import os # For checking environment variables
+import functools
 
 # This app uses the functions with the @app.route() decorator.
 app = Flask(__name__) 
-  
-  
+
+
 # Homepage
 @app.route("/")
 def home_page():
@@ -38,3 +38,50 @@ def env_check():
 @app.route("/redirect")
 def redirecting_page():
     return redirect(url_for("old_view"))
+
+
+### Sessions and Security (Basic Security, anyways) ###
+
+# This is used for signing the session cookies, and should be kept VERY SECRET 
+# (and generated to be MUCH better)
+app.secret_key = b'a_very_secret_key'
+
+# See https://flask.palletsprojects.com/en/2.0.x/quickstart/#sessions
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        # Temporary solution
+        return '''
+        <form method="post">
+            <p><input type=text name=password>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+    elif request.method == "POST":
+        # Extremely Temporary Password
+        if request.form["password"] == "testpassword":
+            # Adjust current session cookie (Python dictionary that is aware it is accessed)
+            session["logged_in"] = True; # This is encrypted and sent
+            return redirect(url_for("secret"))
+        else:
+            return "<h1> WRONG PASSWORD </h1>"
+
+# I am definining my own decorator here, for requiring functions to have a login
+def require_password(wrapped_func):
+    @functools.wraps(wrapped_func)
+    def wrapper_check_login(*args, **kwargs):
+        if "logged_in" not in session or session["logged_in"] is not True:
+            return redirect(url_for("login"))
+        return wrapped_func(*args, **kwargs)
+    return wrapper_check_login
+
+@app.route("/secret")
+@require_password
+def secret():
+    return "<h1> You got to my secret page! </h1>"
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('logged_in', None)
+    return redirect(url_for('home_page'))
